f5016261cae88a025346f7a3c522c8ee6014a77a
relaxdd <awenn2015@gmail.com>
2025-08-22 02:18:32 +0400

    Create translit-cyrillic.js

---
+++ translit-cyrillic.js
@@ -0,0 +1,63 @@
+/**
+ * @param {string} word
+ * @param {boolean|undefined} chpu
+ * @returns {string}
+ */
+function translit_cyrillic(word, chpu = false) {
+  var converter = {
+    а: 'a',
+    б: 'b',
+    в: 'v',
+    г: 'g',
+    д: 'd',
+    е: 'e',
+    ё: 'e',
+    ж: 'zh',
+    з: 'z',
+    и: 'i',
+    й: 'y',
+    к: 'k',
+    л: 'l',
+    м: 'm',
+    н: 'n',
+    о: 'o',
+    п: 'p',
+    р: 'r',
+    с: 's',
+    т: 't',
+    у: 'u',
+    ф: 'f',
+    х: 'h',
+    ц: 'c',
+    ч: 'ch',
+    ш: 'sh',
+    щ: 'sch',
+    ь: '',
+    ы: 'y',
+    ъ: '',
+    э: 'e',
+    ю: 'yu',
+    я: 'ya',
+  };
+
+  if (chpu) {
+    word = word.toLowerCase();
+  }
+
+  var answer = '';
+  for (var i = 0; i < word.length; ++i) {
+    if (converter[word[i]] == undefined) {
+      answer += word[i];
+    } else {
+      answer += converter[word[i]];
+    }
+  }
+
+  if (chpu) {
+    answer = answer.replace(/[^-0-9a-z]/g, '-');
+    answer = answer.replace(/[-]+/g, '-');
+    answer = answer.replace(/^\-|-$/g, '');
+  }
+
+  return answer;
+}
